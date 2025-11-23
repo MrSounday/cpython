7e712f6294763a7ac9e1b0cf974bbda40ed4cf0e
MrSounday <karpovlazar11@gmail.com>
2025-11-23 21:43:09 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+import logging
+from typing import Dict, Tuple, Optional, Set
+from datetime import datetime, timedelta
+from telegram import (
+    Update,
+    ChatPermissions,
+    InlineKeyboard
